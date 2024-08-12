84e55e175c22cdd55e2aa9785a425ba67a9803cd
saurabh-kumar1233 <kumarsaurabh6661@gmail.com>
2024-08-12 10:48:21 +0530

    js commit

--- file1.js
+++ file1.js
@@ -1,3 +1,23 @@
+//save file with js
 
-document.write("hello 52")
-console.log("kanaha");
+// document.write("welcome student","<br>");
+
+// document.write("welcome student","<br>");
+
+// var x=12;
+// var y=4;
+// document.write("addition=",x+y,"<br>")
+// document.write("subtraction=",x-y,"<br>")
+// document.write("multiply=",x*y,"<br>")
+// document.write("division=",x/y,"<br>")
+
+// console.log("hello saiurabh")
+// var x= 120;
+// {
+// }
+// var y=30;
+// console.log(x,y);
+let x=20;
+let y=13
+console.log(x,y);
+
---
+++ file2.js
@@ -0,0 +1,22 @@
+let a =10;
+document.write(a,"<br/>")
+let b = 5;
+document.write(b,"<br/>")
+let sum = a+b;
+document.write(sum,"<br/>")
+let diffrence = a-b;
+document.write(diffrence,"<br/>")
+let product = a*b;
+document.write(product,"<br/>")
+let qutient = a/b;
+document.write(qutient,"<br/>")
+let remainder = a%b;
+document.write(remainder,"<br/>")
+let isequal = (a==b);
+document.write(isequal,"<br/>")
+let isnotequal=(a !=b);
+document.write(isnotequal,"<br/>")
+let isgreter = (a>b);
+document.write(isgreter,"<br/>")
+let logicaland =(a>b&&b<10);
+document.write(logicaland,"<br/>")
