1b9e002c8c3cf68a0a2b4c4d1c7f6b19b17e3b7d
saurabh-kumar1233 <kumarsaurabh6661@gmail.com>
2024-08-13 12:29:38 +0530

    ffile2

--- file2.js
+++ file2.js
@@ -1,22 +1,65 @@
-let a =10;
-document.write(a,"<br/>")
-let b = 5;
-document.write(b,"<br/>")
-let sum = a+b;
-document.write(sum,"<br/>")
-let diffrence = a-b;
-document.write(diffrence,"<br/>")
-let product = a*b;
-document.write(product,"<br/>")
-let qutient = a/b;
-document.write(qutient,"<br/>")
-let remainder = a%b;
-document.write(remainder,"<br/>")
-let isequal = (a==b);
-document.write(isequal,"<br/>")
-let isnotequal=(a !=b);
-document.write(isnotequal,"<br/>")
-let isgreter = (a>b);
-document.write(isgreter,"<br/>")
-let logicaland =(a>b&&b<10);
-document.write(logicaland,"<br/>")
+// let a =10;
+// document.write(a,"<br/>")
+// let b = 5;
+// document.write(b,"<br/>")
+// let sum = a+b;
+// document.write(sum,"<br/>")
+// let diffrence = a-b;
+// document.write(diffrence,"<br/>")
+// let product = a*b;
+// document.write(product,"<br/>")
+// let qutient = a/b;
+// document.write(qutient,"<br/>")
+// let remainder = a%b;
+// document.write(remainder,"<br/>")
+// let isequal = (a==b);
+// document.write(isequal,"<br/>")
+// let isnotequal=(a !=b);
+// document.write(isnotequal,"<br/>")
+// let isgreter = (a>b);
+// document.write(isgreter,"<br/>")
+// let logicaland =(a>b&&b<10);
+// document.write(logicaland,"<br/>")
+
+// let fname="tyagi";
+// document.write("hello" , fname,"<br")
+// let age="5";
+// document.write('age',age,"<br>")
+// let isstudent="true";
+// document.write('student'-${isstudent? "yes":"no"}<br/>')
+// let emptyvalue= "i am engineer";
+// document.write ('about us ${employee|| "write something " }<br>')
+// let uninitialized;
+// document.write(!unitialized && "fill detail completely")
+
+// let age=16;
+// if(age>17)
+// {
+//     document.write("eligible for vote");
+
+// }
+// else
+// {
+//     document.write("not eligible");
+// }
+
+// let number=prompt("enter a number ");
+// if(number>0){
+//     document.write("number is positive");
+// }
+// else if(number<0)
+// {
+//     document.write("number is negative");
+// }
+// else{
+//     document.write("number is zero")
+// }
+
+let number1=prompt("enter your first number");
+let number2=prompt("enter your secand number");
+
+switch (let case)
+{
+   
+
+}
